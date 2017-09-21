68a74301f32310c6783ccb62cd3e16cae29a86f5
johnyhub <31032148+johnyhub@users.noreply.github.com>
2017-09-21 11:32:55 +0200

    ch2

--- js/screenbackground.js
+++ js/screenbackground.js
@@ -15,4 +15,30 @@ $(document).ready(function(){
             }, 1000);  
             
                });
+    $("#ftp").click(function(){
+                  
+                  $(".screen-content").removeClass().addClass("screen-content screen-img3 animated flipInY");
+                  //add tilme out to remove class animation
+                  window.setTimeout( function(){
+                  $(".screen-content").removeClass('animated flipInY');
+            }, 1000);  
+               });
+    $("#tel").click(function(){
+                  
+                  $(".screen-content").removeClass().addClass("screen-content screen-img4 animated flipInY");
+                  //add tilme out to remove class animation
+                  window.setTimeout( function(){
+                  $(".screen-content").removeClass('animated flipInY');
+            }, 1000);  
+               });
+    $("#prem").click(function(){
+                  
+                  $(".screen-content").removeClass().addClass("screen-content screen-img5 animated flipInY");
+                  //add tilme out to remove class animation
+                  window.setTimeout( function(){
+                  $(".screen-content").removeClass('animated flipInY');
+            }, 1000);  
+               });
+    
+    
     });
