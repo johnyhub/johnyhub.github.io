3145bcae5aa9227f5ecb6a1bee638552cf23d46f
johnyhub <31032148+johnyhub@users.noreply.github.com>
2017-10-12 10:37:40 +0200

    ch3

--- js/faqdata.js
+++ js/faqdata.js
@@ -41,21 +41,24 @@ $("a.faq8").on("click", function(){
 
 
 //// DATA CHANGE
-//$(document).ready(function(){
-//    $('div.game-holder').click(function(){
-//        $('div.faq-content').html('<div class="contactbox"><h3>SEND US A MESSAGE</h3><p>Your name: <span class="orange-text">*</span></p> <button class="links-btn newsletter"> <span class="links-blue"><img src="img/icon/contacticonuser.png"><input type="text" placeholder="" name="name" required=""></span><a href=""></a></button><p>Your email: <span class="orange-text">*</span></p> <button class="links-btn newsletter"> <span class="links-blue"><img src="img/icon/contacticonmail.png"><input type="text" placeholder="" name="email" required=""></span><a href=""></a></button><p>Subject: <span class="orange-text">*</span></p> <button class="links-btn newsletter"> <span class="links-blue"><img src="img/icon/contacticonsub.png"><input type="text" placeholder="" name="text" required=""></span><a href=""></a></button><p>What is the problem?</p> <button class="links-btn newsletter textfield"><span class="links-blue"><img src="img/icon/contacticontext.png"><textarea type="text" placeholder="Type your message...." name="email" required=""></textarea></span><a href=""></a></button> <a class="orderbtn report-btn" href=""> REPORT</a></div>');
-//         $('div.faq-content').addClass('animated fadeInLeft');
-//                    
-//    });
-//});
-
 $(document).ready(function(){
     $('div.game-holder').click(function(){
-        $('div.faq-content').load('test.html');
+        $('div.faq-content').html('<div class="contactbox"><h3>SEND US A MESSAGE</h3><p>Your name: <span class="orange-text">*</span></p> <button class="links-btn newsletter"> <span class="links-blue"><img src="img/icon/contacticonuser.png"><input type="text" placeholder="" name="name" required=""></span><a href=""></a></button><p>Your email: <span class="orange-text">*</span></p> <button class="links-btn newsletter"> <span class="links-blue"><img src="img/icon/contacticonmail.png"><input type="text" placeholder="" name="email" required=""></span><a href=""></a></button><p>Subject: <span class="orange-text">*</span></p> <button class="links-btn newsletter"> <span class="links-blue"><img src="img/icon/contacticonsub.png"><input type="text" placeholder="" name="text" required=""></span><a href=""></a></button><p>What is the problem?</p> <button class="links-btn newsletter textfield"><span class="links-blue"><img src="img/icon/contacticontext.png"><textarea type="text" placeholder="Type your message...." name="email" required=""></textarea></span><a href=""></a></button> <a class="orderbtn report-btn" href=""> REPORT</a></div>');
          $('div.faq-content').addClass('animated fadeInLeft');
                     
     });
 });
+
+
+//// DATA CHANGE SA AJAXOM /////
+
+//$(document).ready(function(){
+//    $('div.game-holder').click(function(){
+//        $('div.faq-content').load('test.html');
+//         $('div.faq-content').addClass('animated fadeInLeft');
+//                    
+//    });
+//});
 
 
 
